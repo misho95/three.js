1bb1ac30a59157d1cc4958551654b0869558289c
Misho <chapidze.misho95@gmail.com>
2024-02-06 22:12:19 +0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { TextureLoader } from "three";
 
 function App() {
+  const loader = new TextureLoader();
+  const loadedTexture = loader.load("src/assets/moon.jpg");
+
   return (
     <div className="w-full h-screen ">
       <Canvas>
@@ -11,7 +15,7 @@ function App() {
           <ambientLight intensity={0.1} />
           <directionalLight position={[5, 10, 10]} />
           <sphereGeometry args={[1.5, 64, 32]} />
-          <meshStandardMaterial color={"red"} />
+          <meshStandardMaterial color={"white"} map={loadedTexture} />
         </mesh>
       </Canvas>
     </div>
